5aa7233f9361029b1a964cd409b8c2811144b7b8
Maxamed-Maxamed <20088349@mail.wit.ie>
2025-11-14 18:17:40 +0000

    refactor: update metro.config.js to use ES module syntax and reorganize imports; adjust pnpm-workspace.yaml for built dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,13 @@
-const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+import { createRequire } from 'node:module';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const require = createRequire(import.meta.url);
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config = getSentryExpoConfig(__dirname);
 
-module.exports = config;
+export default config;
